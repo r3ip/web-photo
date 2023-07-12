7c3b130cf33ac7681588f7df218f913e9932efdf
Pier Valenzuela <50628232+r3ip@users.noreply.github.com>
2023-07-12 22:45:23 +0000

    card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import { Card, Text } from 'dracula-ui'
-export const Cards = ({titulo}) => {
+export const Cards = ({ titulo }) => {
 
   const colors = [
     'cyan',
@@ -14,9 +14,20 @@ export const Cards = ({titulo}) => {
 
   return (
     <>
-      <Card className='w-[20rem]' borderColor={colors[0]} p="md" m="md">
+      {/* <Card className='w-[20rem]' borderColor={colors[0]} p="md" m="md">
         <Text >{titulo}</Text>
-      </Card>
+      </Card> */}
+
+      <div className="card w-96 bg-base-100 shadow-xl image-full">
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">{titulo}!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            {/* <button className="btn btn-primary">Buy Now</button> */}
+          </div>
+        </div>
+      </div>
     </>
 
   )
